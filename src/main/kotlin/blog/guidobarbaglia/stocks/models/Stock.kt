552c532e14df2f126baa5b41c421e8566c27e3fd
Kalimaha <guido.barbaglia@gmail.com>
2019-10-31 22:44:14 +1100

    Added price to model for talk

--- src/main/kotlin/blog/guidobarbaglia/stocks/models/Stock.kt
+++ src/main/kotlin/blog/guidobarbaglia/stocks/models/Stock.kt
@@ -4,4 +4,9 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Stock(@Id val id: String? = null, val code: String? = null, val description: String = "Not available.")
+data class Stock(
+    @Id val id: String? = null,
+    val code: String? = null,
+    val description: String = "Not available.",
+    val price: Double = 100 * Math.random()
+)
